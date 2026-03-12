36781afac8cb379d371d8c3ae620df73d3ae6b5f
Justinxiaohao <1418661311@qq.com>
2026-03-12 09:37:57 +0800

    优化全局异常处理，增加具体错误信息返回以提升用户体验

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.bridge.Message;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,24 +19,31 @@ public class GlobalExceptionHandler {
 
     /**
      * 捕获业务异常
+     *
      * @param ex
      * @return
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
+    public Result exceptionHandler(BaseException ex) {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
 
     @ExceptionHandler
-    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
         log.error("异常信息：{}", ex.getMessage());
         String message = ex.getMessage();
-        if(message.contains("Duplicate entry")){
+        if (message.contains("Duplicate entry")) {
             String[] split = message.split(" ");
-            String msg = split[2] + "已存在";
+            String msg = split[2] + MessageConstant.ACCOUNT_EXISTS;
             return Result.error(msg);
+        }
+        else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+
     }
-
-
 }
+
+
+
